6092f5dca2eb03fd159012387c2bb77ffeaa6607
David Hou <david.hou314@gmail.com>
2023-09-09 16:35:43 -0700

    NumNode -> Variable.num in symbolic.py
    1.5%

--- tinygrad/shape/symbolic.py
+++ tinygrad/shape/symbolic.py
@@ -28,7 +28,7 @@ class Node:
   @functools.lru_cache(maxsize=None)  # pylint: disable=method-cache-max-size-none
   def expand(self, idxs:Optional[Tuple[VariableOrNum, ...]]=None) -> List[Node]:
     if idxs is None: idxs = self.expand_idxs()
-    return [self.substitute(dict(zip(idxs, (NumNode(x) for x in rep)))) for rep in Node.iter_idxs(idxs)]
+    return [self.substitute(dict(zip(idxs, (Variable.num(x) for x in rep)))) for rep in Node.iter_idxs(idxs)]
   @staticmethod
   def iter_idxs(idxs:Sequence[VariableOrNum, ...]) -> Iterator[Tuple[int,...]]:
     yield from (x[::-1] for x in product(*[[x for x in range(v.min, v.max + 1)] for v in idxs[::-1]]))
@@ -70,23 +70,23 @@ class Node:
             lhs = Variable.sum(muls)
     return create_node(LtNode(lhs, b))
   def __mul__(self, b:Union[Node, int]):
-    if b == 0: return NumNode(0)
+    if b == 0: return Variable.num(0)
     if b == 1: return self
-    if self.__class__ is NumNode: return NumNode(self.b*b) if isinstance(b, int) else b*self.b
+    if self.__class__ is NumNode: return Variable.num(self.b*b) if isinstance(b, int) else b*self.b
     return create_node(MulNode(self, b.b)) if isinstance(b, NumNode) else create_node(MulNode(self, b))
   def __rmul__(self, b:int): return self*b
 
   # *** complex ops ***
 
   def __rfloordiv__(self, b:int):
-    if self.min > b >= 0: return NumNode(0)
-    if isinstance(self, NumNode): return NumNode(b // self.b)
+    if self.min > b >= 0: return Variable.num(0)
+    if isinstance(self, NumNode): return Variable.num(b // self.b)
     raise RuntimeError(f"not supported: {b} // {self}")
   def __floordiv__(self, b:Union[Node,int], factoring_allowed=True):
     if isinstance(b, Node):
       if b.__class__ is NumNode: return self // b.b
-      if self == b: return NumNode(1)
-      if (b - self).min > 0 and self.min >= 0: return NumNode(0) # b - self simplifies the node
+      if self == b: return Variable.num(1)
+      if (b - self).min > 0 and self.min >= 0: return Variable.num(0) # b - self simplifies the node
       raise RuntimeError(f"not supported: {self} // {b}")
     assert b != 0
     if b < 0: return (self//-b)*-1
@@ -100,17 +100,17 @@ class Node:
     return create_node(DivNode(self, b))
 
   def __rmod__(self, b:int):
-    if self.min > b >= 0: return NumNode(b)
-    if isinstance(self, NumNode): return NumNode(b % self.b)
+    if self.min > b >= 0: return Variable.num(b)
+    if isinstance(self, NumNode): return Variable.num(b % self.b)
     raise RuntimeError(f"not supported: {b} % {self}")
   def __mod__(self, b:Union[Node,int]):
     if isinstance(b, Node):
       if b.__class__ is NumNode: return self % b.b
-      if self == b: return NumNode(0)
+      if self == b: return Variable.num(0)
       if (b - self).min > 0 and self.min >= 0: return self # b - self simplifies the node
       raise RuntimeError(f"not supported: {self} % {b}")
     assert b > 0
-    if b == 1: return NumNode(0)
+    if b == 1: return Variable.num(0)
     if self.min >= 0 and self.max < b: return self
     if self.min < 0: return (self - ((self.min//b)*b)) % b
     return create_node(ModNode(self, b))
@@ -130,7 +130,7 @@ class Node:
   @staticmethod
   def sum(nodes:List[Node]) -> Node:
     nodes = [x for x in nodes if x.max or x.min]
-    if not nodes: return NumNode(0)
+    if not nodes: return Variable.num(0)
     if len(nodes) == 1: return nodes[0]
 
     new_nodes: List[Node] = []
@@ -141,25 +141,25 @@ class Node:
 
     if len(new_nodes) > 1 and len(set([x.a if isinstance(x, MulNode) else x for x in new_nodes])) < len(new_nodes):
       new_nodes = Node.factorize(new_nodes)
-    if num_node_sum: new_nodes.append(NumNode(num_node_sum))
-    return create_rednode(SumNode, new_nodes) if len(new_nodes) > 1 else new_nodes[0] if len(new_nodes) == 1 else NumNode(0)
+    if num_node_sum: new_nodes.append(Variable.num(num_node_sum))
+    return create_rednode(SumNode, new_nodes) if len(new_nodes) > 1 else new_nodes[0] if len(new_nodes) == 1 else Variable.num(0)
 
   @staticmethod
   def ands(nodes:List[Node]) -> Node:
-    if not nodes: return NumNode(1)
+    if not nodes: return Variable.num(1)
     if len(nodes) == 1: return nodes[0]
-    if any(not x for x in nodes): return NumNode(0)
+    if any(not x for x in nodes): return Variable.num(0)
 
     # filter 1s
     nodes = [x for x in nodes if x.min != x.max]
-    return create_rednode(AndNode, nodes) if len(nodes) > 1 else (nodes[0] if len(nodes) == 1 else NumNode(1))
+    return create_rednode(AndNode, nodes) if len(nodes) > 1 else (nodes[0] if len(nodes) == 1 else Variable.num(1))
 
 # 4 basic node types
 
 class Variable(Node):
   def __new__(cls, expr:str, nmin:int, nmax:int, expand=False):
     assert nmin >= 0 and nmin <= nmax
-    if nmin == nmax: return NumNode(nmin)
+    if nmin == nmax: return Variable.num(nmin)
     return super().__new__(cls)
 
   def __init__(self, expr:str, nmin:int, nmax:int, expand=False):
@@ -180,7 +180,7 @@ class NumNode(Node):
 
 def create_node(ret:Node):
   assert ret.min <= ret.max, f"min greater than max! {ret.min} {ret.max} when creating {type(ret)} {ret}"
-  if ret.min == ret.max: return NumNode(ret.min)
+  if ret.min == ret.max: return Variable.num(ret.min)
   return ret
 
 class OpNode(Node):
@@ -242,7 +242,7 @@ class SumNode(RedNode):
     if isinstance(b, SumNode):
       nu_num = sum(node.b for node in self.flat_components if node.__class__ is NumNode)
       de_num = sum(node.b for node in b.flat_components if node.__class__ is NumNode)
-      if nu_num > 0 and de_num and (d:=nu_num//de_num) > 0: return NumNode(d) + (self-b*d) // b
+      if nu_num > 0 and de_num and (d:=nu_num//de_num) > 0: return Variable.num(d) + (self-b*d) // b
     if isinstance(b, Node):
       for x in self.flat_components:
         if x % b == 0: fully_divided.append(x // b)
@@ -306,7 +306,7 @@ class AndNode(RedNode):
   def substitute(self, var_vals: Dict[VariableOrNum, Node]) -> Node:
     subed = []
     for node in self.nodes:
-      if not (sub:=node.substitute(var_vals)): return NumNode(0)
+      if not (sub:=node.substitute(var_vals)): return Variable.num(0)
       subed.append(sub)
     return Variable.ands(subed)
 
