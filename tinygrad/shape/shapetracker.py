db1f71f24eb12e2ce85a2c71fe9a1e9a99ac5199
David Hou <david.hou314@gmail.com>
2024-04-17 16:49:45 -0700

    add the real_strides debug prints

--- tinygrad/shape/shapetracker.py
+++ tinygrad/shape/shapetracker.py
@@ -60,10 +60,13 @@ class ShapeTracker:
     return ShapeTracker(tuple(unbound_views)), merge_dicts(var_vals)
 
   # NOTE: if a stride is not always valid, it will be None
-  def real_strides(self, ignore_valid=False) -> Tuple[Optional[sint], ...]:
+  def real_strides(self, ignore_valid=False, debug=False) -> Tuple[Optional[sint], ...]:
     if len(self.views) == 1 and self.views[-1].mask is None: return self.views[-1].strides
     idxs: List[Node] = [Variable(f"idx{i}", 0, s-1) for i,s in enumerate(self.shape)]
     idx, valid = self.expr_idxs(idxs)
+    if debug and False:
+      print('idx:', idx)
+      print('valid:', valid)
     ret: List[Optional[sint]] = [None] * len(self.views[-1].shape)
     bad_idx_vars: Set[Variable] = set()
     for this_dim in (idx.nodes if isinstance(idx, SumNode) else [idx]):
